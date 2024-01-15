228cd7b906b6f1f8aa1098c43ed62d1e971e098c
Topi Ilmasti <122445154+SUPPlS@users.noreply.github.com>
2024-01-15 15:14:39 +0200

    kotitehtava valmis
    syke saatu nakymaan ja toimimaan

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [alaraja, setAlaraja] = useState(0);
+  const [ylaraja, setYlaraja] = useState(0);
+  
+  const calculate = () => {
+    if (age) {
+      const ala = Math.round((220 - (age)) * 0.65);
+      const yla = Math.round((220 - (age)) * 0.85);
+
+      setAlaraja(ala);
+      setYlaraja(yla);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Age</Text>
+      <TextInput 
+      placeholder='Enter your age' 
+      keyboardType='decimal-pad'
+      value={age}
+      onChangeText={text => setAge(text)}
+      style={styles.field}
+      />
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{`${alaraja}-${ylaraja}bpm`}</Text>
+      <Button title='Calculate' onPress={calculate} />
     </View>
   );
 }
@@ -14,7 +37,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 30,
+    marginLeft: 10,
+    marginRight: 10,
   },
+  field: {
+    marginBottom: 10,
+
+  }
 });
